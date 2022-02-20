d2c6cfe5789686d73c914a43ec7629e853168828
jeton <jetonahmeti69@gmail.com>
2022-02-20 22:23:33 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+   $('.ja-food-slider').slick({
+      autoplay:true,
+     slidesToShow:3,
+     slidesToScroll:1,
+     prevArrow:".prev-btn",
+     nextArrow:".next-btn",
+     responsive:[
+        {
+           breakpoint:992,
+           settings:{
+            slidesToShow:2,
+           }
+        },
+        {
+         breakpoint:768,
+         settings:{
+          slidesToShow:1,
+         }
+      }
+     ]
+
+   });
+
+   $('.ja-nav-trigger').click(function(){
+      $('.ja-site-content-wrapper').toggleClass('scaled');
+   })
+});
